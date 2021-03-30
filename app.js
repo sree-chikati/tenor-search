c7f2896c03520d89136c30cf8efc3c2e7e783f4a
sree-chikati <srechi453.fusdk12@gmail.com>
2021-03-30 16:32:29 -0700

    added search form

--- app.js
+++ app.js
@@ -12,11 +12,10 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
-  })
+    console.log(req.query) // => "{ term: hey" }
+  
+    res.render('home')
+})
 
 
 app.get('/greetings/:name', (req, res) => {
@@ -25,7 +24,8 @@ app.get('/greetings/:name', (req, res) => {
     // render the greetings view, passing along the name
     res.render('greetings', { name });
   })
-// Start Server
+
+  // Start Server
 
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
